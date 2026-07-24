377743bf2f6ae005c9a9e396dcdf3075f111d438
RubenFern <rubenferval@hotmail.com>
2026-07-24 13:36:36 +0200

    feat: script de esquema para la columna username
    Script idempotente 00.0002.0000 generado desde la migración AddUserUsername.

---
+++ schema/00.0002.0000/001-add-user-username.sql
@@ -0,0 +1,19 @@
+﻿START TRANSACTION;
+IF NOT EXISTS(SELECT * FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20260723215222_AddUserUsername')
+BEGIN
+    ALTER TABLE `users` ADD `username` varchar(30) NOT NULL DEFAULT '';
+END;
+
+IF NOT EXISTS(SELECT * FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20260723215222_AddUserUsername')
+BEGIN
+    CREATE UNIQUE INDEX `ux_users_username` ON `users` (`username`);
+END;
+
+IF NOT EXISTS(SELECT * FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20260723215222_AddUserUsername')
+BEGIN
+    INSERT INTO `__EFMigrationsHistory` (`MigrationId`, `ProductVersion`)
+    VALUES ('20260723215222_AddUserUsername', '10.0.10');
+END;
+
+COMMIT;
+
